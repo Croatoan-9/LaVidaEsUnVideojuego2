e4367330870aface4e7393b7fd821835aa06410e
Croatoan-9 <sebse90@hotmail.com>
2017-01-05 21:44:59 +0400

    Initial commit

---
+++ app/src/main/java/com/lavidaesunvideojuego/lavidaesunvideojuego/Comienzo.java
@@ -0,0 +1,13 @@
+package com.lavidaesunvideojuego.lavidaesunvideojuego;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Comienzo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comienzo);
+    }
+}
